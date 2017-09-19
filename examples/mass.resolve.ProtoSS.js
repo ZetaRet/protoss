7ceeb2d17b9d82d6c6e89ccbd82e01b76ec34930
Ivo Yankulovski <ivoyankulovski@gmail.com>
2017-09-19 16:41:13 +0300

    ProtoSS v1.03 [Extension]
    - Class/Interface/Header Manager 
    - Mass Resolve ProtoSS Example

---
+++ examples/mass.resolve.ProtoSS.js
@@ -0,0 +1,184 @@
+/*
+ * Zeta Ret ProtoSS Examples
+ * Mass Resolve Headers/Classes/Interfaces, Generate Interfaces
+ * 
+ * Requires: ProtoSS, protoss.all
+ * 
+ */
+var protoss=new ProtoSS();
+
+/*declare interface description package*/
+window.package("zetaret.global.packages.examples.interfaces").IEvents={
+	IAudioEvent:{getSamples:[Number,Number],getFrequency:[],getBitrate:[],getTime:[],getLength:[],isPlaying:[]},
+	IClipboardEvent:{getText:[],getHTML:[],getClipboardData:[String],setClipboardData:[String,String],clearClipboard:[]},
+	IEvent:{preventBubble:[],preventDefault:[],preventCapture:[],stopPropagation:[],stopImmediatePropagation:[],setEventPhase:[String],setEventType:[String],setEventTarget:["/IEventTarget"],setEventCurrentTarget:["/IEventTarget"],addToTargetMap:["/IEventTarget"],setNativeEvent:[Event],clone:[]},
+	IEventData:{getData:[]},
+	IEventDispatcher:{setEventTarget:["/IEventDispatcher"],addEventListener:[String,Function],removeEventListener:[String,Function],hasEvent:[String],dispatch:[String,"/IEventData"],dispatchEvent:["/IEvent","/IEventData"]},
+	IEventTarget:{},
+	IMouseEvent:{getX:[],getY:[],getLocalX:[],getLocalY:[],getOffsetX:[],getOffsetY:[],getKeys:[],getXY:[]}
+};
+
+/* MouseEvent will be set as subclass of Event later, but it is loaded before Event */
+window.package("zetaret.global.packages.examples.events").internal(
+function MouseEvent(type, target) {
+	var o = this,a=arguments;
+	o.x=0;
+	o.y=0;
+	o.ox=0;
+	o.oy=0;
+	o.lx=0;
+	o.ly=0;
+	o.keys=null;
+	o.super(a,true);
+	var m = {};
+	m.getX=function(){
+		return o.x;
+	};
+	m.getY=function(){
+		return o.x;
+	};
+	m.getLocalX=function(){
+		return o.x;
+	};
+	m.getLocalY=function(){
+		return o.x;
+	};
+	m.getOffsetX=function(){
+		return o.x;
+	};
+	m.getOffsetY=function(){
+		return o.x;
+	};
+	m.getKeys=function(){
+		return o.keys;
+	};
+	m.getXY=function(){
+		return [o.x,o.y,o.lx,o.ly,o.ox,o.oy];
+	};
+	m.setNativeEvent=function(e){
+		o.nativeEvent=e;
+		o.x=e.clientX;
+		o.y=e.clientY;
+		o.ox=e.offsetX;
+		o.oy=e.offsetY;
+		o.lx=e.pageX;
+		o.ly=e.pageY;
+		return o;
+	};
+	o.superize(a, m, true, true);
+	return o;
+}
+);
+ProtoSS.eventProps(zetaret.global.packages.examples.events.MouseEvent,["x","y","ox","oy","lx","ly","keys"]);
+
+/*Top level Event is loaded after one if its subclasses MouseEvent, headers set inheritance on resolve*/
+window.package("zetaret.global.packages.examples.events").internal(
+function Event(type, target) {
+	var o = this,a=arguments;
+	o.super(a,true);
+	var m = {};
+	o.superize(a, m, true, true);
+	return o;
+}
+);
+
+function ZetaRet_Event(type, target){
+	var o = this,a=arguments;
+	o.type=type;
+	o.bubbles=false;
+	o.cancelBubble=false;
+	o.cancelable=false;
+	o.currentTarget=null;
+	o.target=target;
+	o.defaultPrevented=false;
+	o.targetMap=null;
+	o.eventPhase=null;
+	o.time=0;
+	o.creator=null;
+	o.nativeEvent=null;
+	o.super(a);
+	o.cloneprops=o.constructor.eventprops;
+	var m = {};
+	m.preventBubble=function(){
+		o.bubbles=false;
+		return o;
+	};
+	m.preventDefault=function(){
+		o.defaultPrevented=true;
+		return o;
+	};
+	m.preventCapture=function(){
+		o.setEventPhase(ZetaRet_Event.BUBBLE_PHASE);
+		return o;
+	};
+	m.stopPropagation=function(){
+		o.setEventPhase(ZetaRet_Event.NO_PHASE);
+		return o;
+	};
+	m.stopImmediatePropagation=function(){
+		o.setEventPhase(ZetaRet_Event.NULL_PHASE);
+		return o;
+	};
+	m.setEventPhase=function(phase){
+		o.eventPhase=phase;
+		return o;
+	};
+	m.setEventType=function(type){
+		o.type=type;
+		return o;
+	};
+	m.setEventTarget=function(target){
+		o.target=target;
+		return o;
+	};
+	m.setEventCurrentTarget=function(target){
+		o.currentTarget=target;
+		return o;
+	};
+	m.addToTargetMap=function(target){
+		o.targetMap=o.targetMap||[];
+		o.targetMap[o.targetMap.length]=target;
+		return o;
+	};
+	m.setNativeEvent=function(e){
+		o.nativeEvent=e;
+		return o;
+	};
+	m.clone=function(){
+		var c=new o.constructor(o.type);
+		c.bubbles=o.bubbles;
+		c.cancelBubble=o.cancelBubble;
+		c.cancelable=o.cancelable;
+		c.currentTarget=o.currentTarget;
+		c.target=o.target;
+		c.defaultPrevented=o.defaultPrevented;
+		c.targetMap=o.targetMap;
+		c.eventPhase=o.eventPhase;
+		c.time=o.time;
+		c.creator=o.creator;
+		c.nativeEvent=o.nativeEvent;
+		if (o.cloneprops){
+			var cl=o.cloneprops.length;
+			for(var i=0;i<cl;i++){
+				var ck=o.cloneprops[i];
+				c[ck]=o[ck];
+			}
+		}
+		return c;
+	};
+	o.superize(a, m, true, true);
+	return o;
+}
+ZetaRet_Event.BUBBLE_PHASE="bubblePhase";
+ZetaRet_Event.CAPTURE_PHASE="capturePhase";
+ZetaRet_Event.NO_PHASE="noPhase";
+ZetaRet_Event.NULL_PHASE="nullPhase";
+
+/*Headers are always loaded after declaration of ProtoSS classes, usually separated in different files and packages*/
+ProtoSS.headerSuper.push([zetaret.global.packages.examples.events.Event,"ZetaRet_Event"]);/*single super outside package*/
+ProtoSS.headerImplement.push([ZetaRet_Event, "zetaret.global.packages.examples.events.interfaces::IEvent"]);/*add interface as string supername, resolve later, IEvent is not present yet*/
+ProtoSS.headerSuperList2.push([zetaret.global.packages.examples.events.MouseEvent,["zetaret.global.packages.examples.events::Event","zetaret.global.packages.examples.events.interfaces::IMouseEvent"]]);/*MouseEvent extends Event and IMouseEvent, will resolve later*/
+
+protoss.toInterface(zetaret.global.packages.examples.interfaces.IEvents,window.package("zetaret.global.packages.examples.events.interfaces"));/*creates batch interface*/
+protoss.resolveCls(zetaret.global.packages.examples.interfaces.IEvents, window,"zetaret.global.packages.examples.events.interfaces");/*resolves all string classes*/
+protoss.resolveHeaders(window);/*resolves all headers from static props*/
---
+++ protoss/ProtoSS.js
@@ -0,0 +1,150 @@
+/**
+ * Author: Zeta Ret, Ivo Yankulovski
+ * Zeta Ret ProtoSS
+ * ProtoSS Class/Interface/Header Manager
+ * Requires: protoss.all.js v1.02c
+ * Version: 1.03
+ * Date: 2017 
+**/
+function ProtoSS(){
+	var o=this,a=arguments;
+	o.super(a);
+	var m={};
+	m.toInterface=function(i, pack){
+		var imap={};
+		for(var key in i){
+			if (key=="__name")continue;
+			imap[key]=i[key].interface(key);
+			if (pack)pack.internal(imap[key]);
+		}
+		return imap;
+	};
+	m.stringPattern=function(pat, str, pos){
+	    if(!pos)pos=0;
+	    var num=0;
+		while(str.indexOf(pat, pos)==pos){
+			num++;
+			str=str.substr(0,pos)+str.substr(pos+pat.length,str.length);
+		}
+		return [num,str];
+	};
+	m.getSuperCls=function(sname, toppack, path){
+		var many=sname.split("|");
+		var cls=null;
+		for(var i=0;i<many.length;i++){
+			var sn=many[i];
+			var tp=toppack;
+			var sp=o.stringPattern("/",sn);
+			if (path && sp[0]>0){
+				var pathsplit=path.split(".");
+				tp=tp.package(pathsplit.slice(0, pathsplit.length-sp[0]+1).join("."));
+			}
+			var s=sp[1].split("::");
+			if (s.length==1){
+				cls=tp[s[0]];
+				if(cls)return cls;
+			}
+			cls=tp.package(s[0])[s[1]];
+			if(cls)return cls;
+		}
+		return cls;
+	};
+	m.resolveCls=function(s, toppack, path){
+		for(var k in s){
+			if (k=="__name")continue;
+			var sk=s[k];
+			for(var kk in sk){
+				var skk=sk[kk];
+				for(var i=0;i<skk.length;i++){
+					if (skk[i].constructor==String){
+						skk[i]=o.getSuperCls(skk[i], toppack, path);
+					}
+				}
+			}
+		}
+		return o;
+	};
+	m.resolveHeaders=function(toppack, path){
+		var i,j,v,vl,c1,c2;
+		var ia=[];
+		for(i=0;i<ProtoSS.headerImplement.length;i++){
+			v=ProtoSS.headerImplement[i];
+			c1=v[0];
+			c2=o.getSuperCls(v[1], toppack, path);
+			if (c2)c1.implement(c2);
+			else ia.push(v);
+		}
+		ProtoSS.headerImplement=ia;
+		var sa=[];
+		for(i=0;i<ProtoSS.headerSuper.length;i++){
+			v=ProtoSS.headerSuper[i];
+			c1=v[0];
+			c2=o.getSuperCls(v[1], toppack, path);
+			if (c2)c1.setSuper(c2);
+			else sa.push(v);
+		}
+		ProtoSS.headerSuper=sa;
+		var sla=[];
+		var sl;
+		for(i=0;i<ProtoSS.headerSuperList.length;i++){
+			v=ProtoSS.headerSuperList[i];
+			c1=v[0];
+			vl=v[1];
+			sl=[];
+			for(j=0;j<vl.length;j++){
+				c2=o.getSuperCls(vl[j], toppack, path);
+				if (c2)sl.push(c2);
+				else {
+					sla.push(v);
+					break;
+				}
+			}
+			if (sl.length==vl.length){
+				c1.superList(sl);
+			}
+		}
+		ProtoSS.headerSuperList=sla;
+		sla=[];
+		for(i=0;i<ProtoSS.headerSuperList2.length;i++){
+			v=ProtoSS.headerSuperList2[i];
+			c1=v[0];
+			vl=v[1];
+			sl=[];
+			for(j=0;j<vl.length;j++){
+				c2=o.getSuperCls(vl[j], toppack, path);
+				if (c2)sl.push(c2);
+				else {
+					sla.push(v);
+					break;
+				}
+			}
+			if (sl.length==vl.length){
+				c1.superList2(sl);
+			}
+		}
+		ProtoSS.headerSuperList2=sla;
+		return o;
+	};
+	o.superize(a,m,true,true);
+	return o;
+}
+ProtoSS.headerImplement=[];
+ProtoSS.headerSuper=[];
+ProtoSS.headerSuperList=[];
+ProtoSS.headerSuperList2=[];
+ProtoSS.stringmap={};
+ProtoSS.eventProps=function(event, props){
+	var l=props.length;
+	event.eventprops=props;
+	for(var i=0;i<l;i++){
+		var p=props[i];
+		var pu=p.toUpperCase();
+		event[pu]=p;
+		event[pu+"_CHANGE"]=p+"Change";
+	}
+	return event;
+};
+ProtoSS.staticProps=function(stat, props){
+	for(var k in props)stat[k]=props[k];
+	return stat;
+};
