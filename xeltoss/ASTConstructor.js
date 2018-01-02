649c2491bfc177bb9f80db70c0806769b7fd49ec
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-01-02 19:12:12 +0200

    ASTConstructor 1.00
    +ASTConstructor function, used in XeltoSS

---
+++ xeltoss/ASTConstructor.js
@@ -0,0 +1,16 @@
+/**
+ * Author: Zeta Ret, Ivo Yankulovski 
+ * Zeta Ret XeltoSS ASTConstructor 
+ * Generates ASTConstructorInstance of type function 
+ * Version: 1.00 
+ * Date: 2019 
+**/
+function ASTConstructor(key,data,datahandler){
+	var ASTConstructorInstance=function ASTConstructorInstance(){};
+	ASTConstructorInstance.constructor=arguments.callee;
+	ASTConstructorInstance.key=key;
+	ASTConstructorInstance.data=data;
+	ASTConstructorInstance.datahandler=datahandler;
+	ASTConstructorInstance.toString=function toString(){return this.datahandler?this.datahandler(this.key,this.data):this.data.toString()};
+	return ASTConstructorInstance;
+}
