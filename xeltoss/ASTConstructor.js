26a0dc611caea43d6354325d0c72eba2e8c21d60
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-01-03 22:15:44 +0200

    ASTConstructor 1.01
    +selfcast

--- xeltoss/ASTConstructor.js
+++ xeltoss/ASTConstructor.js
@@ -2,11 +2,14 @@
  * Author: Zeta Ret, Ivo Yankulovski 
  * Zeta Ret XeltoSS ASTConstructor 
  * Generates ASTConstructorInstance of type function 
- * Version: 1.00 
+ * Version: 1.01 
  * Date: 2018 
 **/
 function ASTConstructor(key,data,datahandler){
-	var ASTConstructorInstance=function ASTConstructorInstance(){};
+	var ASTConstructorInstance=function ASTConstructorInstance(cast){
+		ASTConstructorInstance.data=cast; 
+		return ASTConstructorInstance.toString();
+	};
 	ASTConstructorInstance.constructor=arguments.callee;
 	ASTConstructorInstance.key=key;
 	ASTConstructorInstance.data=data;
