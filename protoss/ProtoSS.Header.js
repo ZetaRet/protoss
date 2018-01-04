59b85d730c621ad45b6f9621f8181f62bda11393
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-01-04 17:33:17 +0200

    IProtoSS
    +ProtoSS Interface & Header

---
+++ protoss/ProtoSS.Header.js
@@ -0,0 +1,4 @@
+window.package("zetaret.global.packages.protoss.protoss.interfaces").internal(
+		window.package("zetaret.global.packages.protoss.protoss").IProtoSS.interface("IProtoSS")
+);
+ProtoSS.implement(window.package("zetaret.global.packages.protoss.protoss.interfaces").IProtoSS);
