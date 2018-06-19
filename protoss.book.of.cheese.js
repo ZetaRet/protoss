5805c1a9354e87b44efc013cdb4de1a17b4b7633
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-06-19 18:39:21 +0300

    ProtoSS Book of Cheese v1.04
    *Array.prototype.unique may use performance Set replica check

--- protoss.book.of.cheese.js
+++ protoss.book.of.cheese.js
@@ -2,8 +2,8 @@
  * Author: Zeta Ret, Ivo Yankulovski
  * Load the book before protoss.*.js to cheese up the browser
  * Provides backwards compatibility in old browsers 
- * Version: 1.03 
- * Date: 2018 
+ * Version: 1.04 
+ * Date: 2018 - Today
 **/
 
 /*adds name getter to Function prototype, cache to aname as it is used in protoss*/
@@ -25,35 +25,41 @@ if(!Function.prototype.apply){
 		if(!f)f=this;
 		if(!args)return f.call(scope);
 		switch (args.length) {
-	        case 0: return f.call(scope);
-	        case 1: return f.call(scope, args[0]);
-	        case 2: return f.call(scope, args[0], args[1]);
-	        case 3: return f.call(scope, args[0], args[1], args[2]);
-	        case 4: return f.call(scope, args[0], args[1], args[2], args[3]);
-	        case 5: return f.call(scope, args[0], args[1], args[2], args[3], args[4]);
-	        case 6: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5]);
-	        case 7: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
-	        case 8: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
-	        case 9: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
-	        case 10: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
-	        case 11: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10]);
-	        case 12: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11]);
-	        case 13: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12]);
-	        case 14: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13]);
-	    }
+			case 0: return f.call(scope);
+			case 1: return f.call(scope, args[0]);
+			case 2: return f.call(scope, args[0], args[1]);
+			case 3: return f.call(scope, args[0], args[1], args[2]);
+			case 4: return f.call(scope, args[0], args[1], args[2], args[3]);
+			case 5: return f.call(scope, args[0], args[1], args[2], args[3], args[4]);
+			case 6: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5]);
+			case 7: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+			case 8: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+			case 9: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
+			case 10: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
+			case 11: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10]);
+			case 12: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11]);
+			case 13: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12]);
+			case 14: return f.call(scope, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13]);
+		}
 	};
 }
 /*adds unique Array prototype*/
 if(!Array.prototype.unique){
-	Array.prototype.unique=function(){
-		var u=[],l=this.length,i;
-		for (i=0;i<l;i++){
-	    	if(u.indexOf(this[i])===-1){
-	        	u.push(this[i]);
-	    	}
-		}
-	    return u;
-	};
+	if(Array.from && window['Set'] && Set && Set.prototype.entries && Set.prototype.values){
+		Array.prototype.unique=function(){
+			return Array.from(new Set(this));
+		};
+	} else {
+		Array.prototype.unique=function(){
+			var u=[],l=this.length,i;
+			for (i=0;i<l;i++){
+				if(u.indexOf(this[i])===-1){
+					u.push(this[i]);
+				}
+			}
+			return u;
+		};
+	}
 }
 /*adds padStart and padEnd String prototype*/
 if(!String.prototype.padStart){
