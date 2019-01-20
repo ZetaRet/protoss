0343b69fbcb9d2be70e7a268b7807b0194063e4e
Ivo Yankulovski <ivoyankulovski@gmail.com>
2019-01-20 23:52:55 +0200

    Create SkytoSS.Header.js

---
+++ skytoss/SkytoSS.Header.js
@@ -0,0 +1,4 @@
+window.package("zetaret.global.packages.protoss.skytoss.interfaces").internal(
+		window.package("zetaret.global.packages.protoss.skytoss").ISkytoSS.interface("ISkytoSS")
+);
+SkytoSS.implement(window.package("zetaret.global.packages.protoss.skytoss.interfaces").ISkytoSS);
