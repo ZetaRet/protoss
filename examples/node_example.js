879ae431cb5c4c08e0c3f8dfe65dbd931daff3d8
Ivo Yankulovski <ivoyankulovski@gmail.com>
2019-09-15 16:38:52 +0300

    Create node_example.js

---
+++ examples/node_example.js
@@ -0,0 +1,11 @@
+/*
+Place node_example.js and protoss repository in one folder.
+Open terminal and run:
+node node_example.js
+*/
+require('./protoss/protoss.node.module.js').protossDefaultInit();
+console.log(window);
+console.log(document);
+console.log(ProtoSS);
+console.log(XeltoSS);
+console.log(SkytoSS);
